f64629548efeaf2c7f44c95e85591cc852b4b633
Emil Freme <emil@1000fps.co>
2023-05-23 15:48:32 -0300

    Back end simple validation

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
+import re
 
 app = Flask(__name__)
 
@@ -8,8 +9,34 @@ def index():
 
 @app.route("/login", methods={"GET", "POST"})
 def login():
+    if(request.method == "POST"):
+        username = request.form["username"]
+        password = request.form["pwd"]
+        if not login_validation(username, password):
+            flash("Credênciais Invalidas")
+            return redirect("/login") 
     return render_template("login.html")
 
 @app.route("/usuario")
 def usuario():
     return "Usuário"
+
+
+def login_validation(username, password):
+
+    valida_apenasAlphanumericos = r"^[a-zA-Z0-9]+$"
+    if not re.search(valida_apenasAlphanumericos, username):
+        return False
+
+    valida_maiusculas = r"[A-Z]"
+    valida_caracteresEspeciais = r"[!@#$%^&*()_+-=]"
+
+    if not re.search(valida_maiusculas, password):
+        return False
+
+    if not re.search(valida_caracteresEspeciais, password):
+        return False
+
+    return True
+
+
